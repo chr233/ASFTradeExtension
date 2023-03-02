b7dfcde9c6d41282557bda232688888480c76a71
Chr_ <chr@chrxw.com>
2023-03-02 23:27:17 +0800

    update

--- CardTradeExtension/Core/Command.cs
+++ CardTradeExtension/Core/Command.cs
@@ -11,42 +11,6 @@ namespace CardTradeExtension.Core
         /// 获取成套卡牌套数列表
         /// </summary>
         /// <param name="bot"></param>
-        /// <param name="keyword"></param>
-        /// <param name="page"></param>
-        /// <param name="num"></param>
-        /// <returns></returns>
-        private static async Task<string?> ResponseGetFullSetList(Bot bot, string? keyword, uint page = 1, uint num = 30)
-        {
-            if (!bot.IsConnectedAndLoggedOn)
-            {
-                return bot.FormatBotResponse(Strings.BotNotConnected);
-            }
-
-            // We require to fetch whole inventory as a list here, as we need to know the order for calculating index and previousAssetID
-            List<Asset> inventory;
-
-            try
-            {
-                inventory = await bot.ArchiWebHandler.GetInventoryAsync(0, 753, 6).ToListAsync().ConfigureAwait(false);
-            }
-            catch (HttpRequestException e)
-            {
-                ASFLogger.LogGenericWarningException(e);
-                return null;
-            }
-            catch (Exception e)
-            {
-                ASFLogger.LogGenericException(e);
-                return null;
-            }
-
-            return "";
-        }
-
-        /// <summary>
-        /// 获取成套卡牌套数列表
-        /// </summary>
-        /// <param name="bot"></param>
         /// <returns></returns>
         internal static async Task<string?> ResponseGetFullSetList(Bot bot, string? extraArgs)
         {
@@ -87,8 +51,7 @@ namespace CardTradeExtension.Core
                     }
                 }
             }
-
-            return await ResponseGetFullSetList(bot, keyword, page, num).ConfigureAwait(false);
+            return await Handler.ResponseGetFullSetList(bot, keyword, page, num).ConfigureAwait(false);
         }
 
         /// <summary>
@@ -117,5 +80,81 @@ namespace CardTradeExtension.Core
 
             return responses.Count > 0 ? string.Join(Environment.NewLine, responses) : null;
         }
+
+        /// <summary>
+        /// 获取成套卡牌套数列表
+        /// </summary>
+        /// <param name="bot"></param>
+        /// <returns></returns>
+        internal static async Task<string?> ResponseGetCardSetCountOfGame(Bot bot, string appIds)
+        {
+            string? keyword = null;
+            uint page = 1;
+            uint num = 30;
+
+            if (!string.IsNullOrEmpty(appIds))
+            {
+                var args = appIds.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < args.Length; i += 2)
+                {
+                    string key = args[i];
+                    string value = args[i + 1];
+                    if (key != null)
+                    {
+                        switch (key)
+                        {
+                            case "-k":
+                            case "-key":
+                                keyword = value;
+                                break;
+                            case "-p":
+                            case "-page":
+                                if (uint.TryParse(value, out uint p))
+                                {
+                                    page = p;
+                                }
+                                break;
+                            case "-n":
+                            case "-num":
+                                if (uint.TryParse(value, out uint n))
+                                {
+                                    num = n;
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            return await Handler.ResponseGetSetCountOfGame(bot, keyword, page, num).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// 获取成套卡牌套数 (多个Bot)
+        /// </summary>
+        /// <param name="botNames"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        internal static async Task<string?> ResponseGetCardSetCountOfGame(string botNames, string appIds)
+        {
+            if (string.IsNullOrEmpty(botNames))
+            {
+                throw new ArgumentNullException(nameof(botNames));
+            }
+
+            HashSet<Bot>? bots = Bot.GetBots(botNames);
+
+            if (bots == null || bots.Count == 0)
+            {
+                return FormatStaticResponse(string.Format(Strings.BotNotFound, botNames));
+            }
+
+            IList<string?> results = await Utilities.InParallel(bots.Select(bot => ResponseGetCardSetCountOfGame(bot, appIds))).ConfigureAwait(false);
+
+            List<string> responses = new(results.Where(result => !string.IsNullOrEmpty(result))!);
+
+            return responses.Count > 0 ? string.Join(Environment.NewLine, responses) : null;
+        }
+
+
     }
 }
---
+++ CardTradeExtension/Core/Handler.cs
@@ -0,0 +1,75 @@
+using ArchiSteamFarm.Core;
+using ArchiSteamFarm.Localization;
+using ArchiSteamFarm.Steam;
+using ArchiSteamFarm.Steam.Data;
+
+namespace CardTradeExtension.Core
+{
+    internal static class Handler
+    {
+        /// <summary>
+        /// 获取成套卡牌套数列表
+        /// </summary>
+        /// <param name="bot"></param>
+        /// <param name="keyword"></param>
+        /// <param name="page"></param>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        internal static async Task<string?> ResponseGetFullSetList(Bot bot, string? keyword, uint page = 1, uint num = 30)
+        {
+            if (!bot.IsConnectedAndLoggedOn)
+            {
+                return bot.FormatBotResponse(Strings.BotNotConnected);
+            }
+
+            // We require to fetch whole inventory as a list here, as we need to know the order for calculating index and previousAssetID
+            List<Asset> inventory;
+
+            try
+            {
+                inventory = await bot.ArchiWebHandler.GetInventoryAsync(0, 753, 6).ToListAsync().ConfigureAwait(false);
+            }
+            catch (HttpRequestException e)
+            {
+                ASFLogger.LogGenericWarningException(e);
+                return null;
+            }
+            catch (Exception e)
+            {
+                ASFLogger.LogGenericException(e);
+                return null;
+            }
+
+            return "";
+        }
+
+
+        internal static async Task<string?> ResponseGetSetCountOfGame(Bot bot,uint appId)
+        {
+            if (!bot.IsConnectedAndLoggedOn)
+            {
+                return bot.FormatBotResponse(Strings.BotNotConnected);
+            }
+
+            // We require to fetch whole inventory as a list here, as we need to know the order for calculating index and previousAssetID
+            List<Asset> inventory;
+
+            try
+            {
+                inventory = await bot.ArchiWebHandler.GetInventoryAsync(0, 753, 6).ToListAsync().ConfigureAwait(false);
+            }
+            catch (HttpRequestException e)
+            {
+                ASFLogger.LogGenericWarningException(e);
+                return null;
+            }
+            catch (Exception e)
+            {
+                ASFLogger.LogGenericException(e);
+                return null;
+            }
+
+            return "";
+        }
+    }
+}

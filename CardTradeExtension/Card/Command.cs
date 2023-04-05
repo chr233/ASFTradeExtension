07b75df838cb0df2e01621ff38be4bead9e17f6e
Chr_ <chr@chrxw.com>
2023-04-05 21:20:21 +0800

    UPDATE

--- CardTradeExtension/Card/Command.cs
+++ CardTradeExtension/Card/Command.cs
@@ -2,6 +2,7 @@ using ArchiSteamFarm.Core;
 using ArchiSteamFarm.Localization;
 using ArchiSteamFarm.Steam;
 using ArchiSteamFarm.Steam.Data;
+using ArchiSteamFarm.Steam.Security;
 using SteamKit2;
 using System.Text;
 

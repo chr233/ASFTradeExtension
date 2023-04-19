0165b02e977f7dc45a9cdfcdbda67304414c1f5f
chr_ <chr@chrxw.com>
2023-04-19 19:19:56 +0800

    update

--- CardTradeExtension/CSGO/WebRequests.cs
+++ CardTradeExtension/CSGO/WebRequests.cs
@@ -1,4 +1,4 @@
-﻿using AngleSharp.Dom;
+using AngleSharp.Dom;
 using ArchiSteamFarm.Core;
 using ArchiSteamFarm.Steam;
 using ArchiSteamFarm.Steam.Data;
@@ -6,57 +6,56 @@ using ArchiSteamFarm.Web.Responses;
 using CardTradeExtension.Data;
 
 namespace CardTradeExtension.CSGO;
+
+internal static class WebRequests
 {
-    internal static class WebRequests
+    /// <summary>
+    /// 读取交易链接
+    /// </summary>
+    /// <param name="bot"></param>
+    /// <returns></returns>
+    internal static async Task<string?> GetTradeToken(Bot bot)
     {
-        /// <summary>
-        /// 读取交易链接
-        /// </summary>
-        /// <param name="bot"></param>
-        /// <returns></returns>
-        internal static async Task<string?> GetTradeToken(Bot bot)
+        Uri request = new(SteamCommunityURL, $"/profiles/{bot.SteamID}/tradeoffers/privacy");
+        HtmlDocumentResponse? response = await bot.ArchiWebHandler.UrlGetToHtmlDocumentWithSession(request, referer: SteamStoreURL).ConfigureAwait(false);
+
+        if (response?.Content == null)
         {
-            Uri request = new(SteamCommunityURL, $"/profiles/{bot.SteamID}/tradeoffers/privacy");
-            HtmlDocumentResponse? response = await bot.ArchiWebHandler.UrlGetToHtmlDocumentWithSession(request, referer: SteamStoreURL).ConfigureAwait(false);
-
-            if (response?.Content == null)
-            {
-                return null;
-            }
-
-            var inputEle = response.Content.SelectSingleNode<IElement>("//input[@id='trade_offer_access_url']");
-
-            string? tradeLink = inputEle?.GetAttribute("value");
-            if (string.IsNullOrEmpty(tradeLink))
-            {
-                return null;
-            }
-
-            var match = RegexUtils.MatchTradeLink().Match(tradeLink);
-            return match.Success ? match.Groups[2].Value : null;
+            return null;
         }
 
-        /// <summary>
-        /// 出售指定物品
-        /// </summary>
-        /// <param name="bot"></param>
-        /// <param name="asset"></param>
-        /// <param name="price"></param>
-        /// <returns></returns>
-        internal static async Task<SellItemResponse?> SellItem(Bot bot, Asset asset, ulong price)
+        var inputEle = response.Content.SelectSingleNode<IElement>("//input[@id='trade_offer_access_url']");
+
+        string? tradeLink = inputEle?.GetAttribute("value");
+        if (string.IsNullOrEmpty(tradeLink))
         {
-            Uri request = new(SteamCommunityURL, "/market/sellitem/");
-            Dictionary<string, string> data = new(6) {
-                { "appid", asset.AppID.ToString() },
-                { "contextid", asset.ContextID.ToString() },
-                { "assetid", asset.AssetID.ToString() },
-                { "amount", asset.Amount.ToString() },
-                { "price", price.ToString() },
-            };
-
-            var response = await bot.ArchiWebHandler.UrlPostToJsonObjectWithSession<SellItemResponse>(request, data: data).ConfigureAwait(false);
-
-            return response?.Content;
+            return null;
         }
+
+        var match = RegexUtils.MatchTradeLink().Match(tradeLink);
+        return match.Success ? match.Groups[2].Value : null;
+    }
+
+    /// <summary>
+    /// 出售指定物品
+    /// </summary>
+    /// <param name="bot"></param>
+    /// <param name="asset"></param>
+    /// <param name="price"></param>
+    /// <returns></returns>
+    internal static async Task<SellItemResponse?> SellItem(Bot bot, Asset asset, ulong price)
+    {
+        Uri request = new(SteamCommunityURL, "/market/sellitem/");
+        Dictionary<string, string> data = new(6) {
+            { "appid", asset.AppID.ToString() },
+            { "contextid", asset.ContextID.ToString() },
+            { "assetid", asset.AssetID.ToString() },
+            { "amount", asset.Amount.ToString() },
+            { "price", price.ToString() },
+        };
+
+        var response = await bot.ArchiWebHandler.UrlPostToJsonObjectWithSession<SellItemResponse>(request, data: data).ConfigureAwait(false);
+
+        return response?.Content;
     }
 }
